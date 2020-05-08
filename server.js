476c84fa8a6de3939c07fe66e00a789bb9b3d57e
Shawn Yu <shawnyudesign@gmail.com>
2020-05-07 22:25:51 -0400

    sdsds

--- server.js
+++ server.js
@@ -26,9 +26,9 @@ if (process.env.NODE_ENV === "production") {
 //   app.use(express.static("build"));
 
 
-//   app.get("*", (req, res) => {
-//        res.sendFile(path.resolve(__dirname,  "build", "index.html"));
-//    });
+  app.get("*", (req, res) => {
+       res.sendFile(path.join(__dirname,  "reactportfolioshawnyu/build", "index.html"));
+   });
 
  }
 
